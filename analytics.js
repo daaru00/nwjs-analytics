1d67c28c6cac530df2658ae1a63c7c14687d508f
PROPHESSOR <xenteze@gmail.com>
2017-08-16 02:38:14 +0300

    Fix
    Сохранение анонимного ID у юзверя, что бы не миллионилось в глазах

--- analytics.js
+++ analytics.js
@@ -4,50 +4,51 @@
  * github: https://github.com/Daaru00/nwjs-analytics
  */
 
-
-var analytics = {
+var ga = {
     apiVersion: '1',
-    trackID: 'UA-XXXXXXXX-X',
-    clientID: null,
-    userID: null,
-	appName: 'appname',
-	appVersion: '1.0.0',
-	debug: false,
-	performanceTracking: true,
-	errorTracking: true,
-	userLanguage: "en",
+    trackID: 'UA-XXXXXXXXX-X',
+    clientID: null || localStorage.cid,
+    userID: null || localStorage.uid,
+    appName: 'appname',
+    appVersion: '1.0.0',
+    debug: false,
+    performanceTracking: true,
+    errorTracking: true,
+    userLanguage: "ru",
     currency: "EUR",
     lastScreenName: '',
 
-    sendRequest: function(data, callback){
-        if(!this.clientID || this.clientID == null)
-            this.clientID = this.generateClientID();
+    sendRequest: function (data, callback) {
+        if (!this.clientID || this.clientID == null) {
+            this.clientID = localStorage.cid = this.generateClientID();
+        }
 
-        if(!this.userID || this.userID == null)
-            this.userID = this.generateClientID();
+        if (!this.userID || this.userID == null) {
+            this.userID = localStorage.uid = this.generateClientID();
+        }
 
-        var postData = "v="+this.apiVersion
-                        +"&tid="+this.trackID
-                        +"&cid="+this.clientID
-                        +"&uid="+this.userID
-                        +"&an="+this.appName
-                        +"&av="+this.appVersion
-                        +"&sr="+this.getScreenResolution()
-                        +"&vp="+this.getViewportSize()
-                        +"&sd="+this.getColorDept()
-                        +"&ul="+this.userLanguage
-                        +"&ua="+this.getUserAgent()
-                        +"&ds=app";
+        var postData = "v=" + this.apiVersion
+            + "&tid=" + this.trackID
+            + "&cid=" + this.clientID
+            + "&uid=" + this.userID
+            + "&an=" + this.appName
+            + "&av=" + this.appVersion
+            + "&sr=" + this.getScreenResolution()
+            + "&vp=" + this.getViewportSize()
+            + "&sd=" + this.getColorDept()
+            + "&ul=" + this.userLanguage
+            + "&ua=" + this.getUserAgent()
+            + "&ds=app";
 
-        Object.keys(data).forEach(function(key) {
+        Object.keys(data).forEach(function (key) {
             var val = data[key];
-            if(typeof val != "undefined")
-                postData += "&"+key+"="+val;
+            if (typeof val != "undefined")
+                postData += "&" + key + "=" + val;
         });
 
         var http = new XMLHttpRequest();
         var url = "https://www.google-analytics.com";
-        if(!this.debug)
+        if (!this.debug)
             url += "/collect";
         else
             url += "/debug/collect";
@@ -56,41 +57,39 @@ var analytics = {
 
         http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-        http.onreadystatechange = function() {
-            if(analytics.debug)
+        http.onreadystatechange = function () {
+            if (analytics.debug)
                 console.log(http.response);
 
-            if(http.readyState == 4 && http.status == 200) {
-                if(callback)
+            if (http.readyState == 4 && http.status == 200) {
+                if (callback)
                     callback(true);
             }
-            else
-            {
-                if(callback)
+            else {
+                if (callback)
                     callback(false);
             }
         }
         http.send(postData);
     },
-    generateClientID: function()
-    {
+    generateClientID: function () {
         var id = "";
         var possibilities = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for( var i=0; i < 5; i++ )
+        for (var i = 0; i < 5; i++)
             id += possibilities.charAt(Math.floor(Math.random() * possibilities.length));
         return id;
     },
-    getScreenResolution: function(){
-        return screen.width+"x"+screen.height;
+    getScreenResolution: function () {
+        return screen.width + "x" + screen.height;
     },
-    getColorDept: function(){
-        return screen.colorDepth+"-bits";
+    getColorDept: function () {
+        return screen.colorDepth + "-bits";
     },
-    getUserAgent: function(){
+    getUserAgent: function () {
         return navigator.userAgent;
     },
-    getViewportSize: function(){
-        return window.screen.availWidth+"x"+window.screen.availHeight;
+    getViewportSize: function () {
+        return window.screen.availWidth + "x" + window.screen.availHeight;
     },
 
     /*
@@ -98,84 +97,83 @@ var analytics = {
      * [https://developers.google.com/analytics/devguides/collection/protocol/v1/devguide]
      */
 
-    screenView: function(screename){
+    screenView: function (screename) {
         var data = {
-			't' : 'screenview',
-			'cd' : screename
-		}
-		this.sendRequest(data);
+            't': 'screenview',
+            'cd': screename
+        }
+        this.sendRequest(data);
         this.lastScreenName = screename;
     },
-    event: function(category, action, label, value){
+    event: function (category, action, label, value) {
         var data = {
-			't' : 'event',
-			'ec' : category,
-			'ea' : action,
-			'el' : label,
-			'ev' : value,
-            'cd' : this.lastScreenName,
-		}
-		this.sendRequest(data);
+            't': 'event',
+            'ec': category,
+            'ea': action,
+            'el': label,
+            'ev': value,
+            'cd': this.lastScreenName,
+        }
+        this.sendRequest(data);
     },
-    exception: function(msg, fatal){
+    exception: function (msg, fatal) {
         var data = {
-			't' : 'exception',
-			'exd' : msg,
-			'exf' : fatal || 0
-		}
-		this.sendRequest(data);
+            't': 'exception',
+            'exd': msg,
+            'exf': fatal || 0
+        }
+        this.sendRequest(data);
     },
-    timing: function(category, variable, time, label){
+    timing: function (category, variable, time, label) {
 
         var data = {
-			't' : 'timing',
-			'utc' : category,
-			'utv' : variable,
-			'utt' : time,
-			'utl' : label,
-		}
-		this.sendRequest(data);
+            't': 'timing',
+            'utc': category,
+            'utv': variable,
+            'utt': time,
+            'utl': label,
+        }
+        this.sendRequest(data);
     },
-    ecommerce:{
+    ecommerce: {
         transactionID: false,
-        generateTransactionID: function()
-        {
+        generateTransactionID: function () {
             var id = "";
             var possibilities = "0123456789";
-            for( var i=0; i < 5; i++ )
+            for (var i = 0; i < 5; i++)
                 id += possibilities.charAt(Math.floor(Math.random() * possibilities.length));
             return id;
         },
-        transaction: function(total, items){
+        transaction: function (total, items) {
             var t_id = "";
-            if(!this.ecommerce.transactionID)
+            if (!this.ecommerce.transactionID)
                 t_id = this.ecommerce.generateTransactionID();
             else
                 t_id = this.ecommerce.transactionID;
 
             var data = {
-                't' : 'transaction',
-                'ti' : t_id,
-                'tr' : total,
-                'cu' : this.currency,
+                't': 'transaction',
+                'ti': t_id,
+                'tr': total,
+                'cu': this.currency,
             }
             this.sendRequest(data);
 
-            items.forEach(function(item){
+            items.forEach(function (item) {
                 var data = {
-                    't' : 'item',
-                    'ti' : t_id,
-                    'in' : item.name,
-                    'ip' : item.price,
-                    'iq' : item.qty,
-                    'ic' : item.id,
-                    'cu' : this.currency
+                    't': 'item',
+                    'ti': t_id,
+                    'in': item.name,
+                    'ip': item.price,
+                    'iq': item.qty,
+                    'ic': item.id,
+                    'cu': this.currency
                 }
                 this.sendRequest(data);
             })
         }
     },
-    custom: function(data){
+    custom: function (data) {
         this.sendRequest(data);
     }
 }
@@ -184,20 +182,21 @@ var analytics = {
  * Performance Tracking
  */
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
 
-    if(analytics.performanceTracking)
-    {
-        setTimeout(function() {
+    if (analytics.performanceTracking) {
+        setTimeout(function () {
             var timing = window.performance.timing;
             var userTime = timing.loadEventEnd - timing.navigationStart;
 
             analytics.timing("performance", "pageload", userTime);
 
-          }, 0);
+        }, 0);
     }
 
 }, false);
+
+var analytics = ga;
 
 /*
  * Error Reporting
@@ -211,9 +210,8 @@ window.onerror = function (msg, url, lineNo, columnNo, error) {
         'Error object: ' + JSON.stringify(error)
     ].join(' - ');
 
-    if(analytics.errorTracking)
-    {
-        setTimeout(function() {
+    if (analytics.errorTracking) {
+        setTimeout(function () {
             analytics.exception(message.toString());
         }, 0);
     }
